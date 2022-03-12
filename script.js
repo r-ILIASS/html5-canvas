bb1f4d6a0efb9031bf5c3ac494782ca9d90aa08d
r-ILIASS <iliass.root@gmail.com>
2022-03-12 17:18:06 +0100

    day1

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d"); // Canvas context
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+const particlesArray = [];
 
 window.addEventListener("resize", () => {
   canvas.width = window.innerWidth;
@@ -13,39 +14,61 @@ const mouse = {
   x: null,
   y: null,
 };
-
-// Draw circle on click
 canvas.addEventListener("click", (e) => {
   mouse.x = e.x;
   mouse.y = e.y;
-
-  draw(mouse.x, mouse.y);
 });
-
-// Paint brush
 canvas.addEventListener("mousemove", (e) => {
   mouse.x = e.x;
   mouse.y = e.y;
-
-  brush(mouse.x, mouse.y);
 });
 
-// Basic draw function
-const draw = (x, y) => {
-  ctx.fillStyle = "red";
-  ctx.strokeStyle = "white";
-  ctx.lineWidth = 2;
-  ctx.beginPath();
-  ctx.arc(x, y, 50, 0, Math.PI * 2);
-  ctx.stroke();
+// Particle blueprint
+class Particle {
+  constructor() {
+    // this.x = mouse.x;
+    // this.y = mouse.y;
+    this.x = Math.random() * canvas.width;
+    this.y = Math.random() * canvas.height;
+    this.size = Math.random() * 5 + 1;
+    this.speedX = Math.random() * 3 - 1.5;
+    this.speedY = Math.random() * 3 - 1.5;
+  }
+
+  update() {
+    this.x += this.speedX;
+    this.y += this.speedY;
+  }
+
+  draw() {
+    ctx.fillStyle = "cyan";
+    ctx.lineWidth = 2;
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, 30, 0, Math.PI * 2);
+    ctx.fill();
+  }
+}
+
+const handleParticles = () => {
+  particlesArray.forEach((particle) => {
+    particle.update();
+    particle.draw();
+  });
 };
 
-// Basic brush function
-const brush = (x, y) => {
-  ctx.fillStyle = "red";
-  ctx.strokeStyle = "white";
-  ctx.lineWidth = 2;
-  ctx.beginPath();
-  ctx.arc(x, y, 3, 0, Math.PI * 2);
-  ctx.fill();
+// create a 100 particles
+const init = () => {
+  for (let i = 0; i < 100; i++) {
+    particlesArray.push(new Particle());
+  }
 };
+init();
+
+// animate
+const animate = () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  handleParticles();
+  requestAnimationFrame(animate);
+};
+
+animate();
